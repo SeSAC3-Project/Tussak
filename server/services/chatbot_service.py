3ed065ed64536e8de7f5f0c1cee7b1afaed0ba09
HwaJin9718 <165185641+HwaJin9718@users.noreply.github.com>
2025-08-26 11:18:36 +0900

    chatbot_service 내 내부 변수 캡슐화 (private 변수로 수정)

--- server/services/chatbot_service.py
+++ server/services/chatbot_service.py
@@ -14,34 +14,34 @@ API_KEY = os.getenv("OPENAI_API_KEY")
 class ChatbotService:
 
     # 템플릿 정의
-    system_template = '''
+    __system_template = '''
     당신은 주식을 처음 접하는 사람도 이해하기 쉽도록 주식 용어를 설명해주는 주식 전문가 입니다.
     사용자가 물어보는 주식 용어에 대해 처음 접하는 사람도 이해하기 쉽게 답변해주세요.
     답변은 주식 용어에 대한 설명만 하고, 추가적인 설명은 하지 마세요.
     '''
 
-    user_template = '{user_request}'
+    __user_template = '{user_request}'
 
-    prompt = ChatPromptTemplate.from_messages([
-        SystemMessagePromptTemplate.from_template(system_template),
-        HumanMessagePromptTemplate.from_template(user_template)
+    __prompt = ChatPromptTemplate.from_messages([
+        SystemMessagePromptTemplate.from_template(__system_template),
+        HumanMessagePromptTemplate.from_template(__user_template)
     ])
 
     # 모델 정의
-    llm = ChatOpenAI(
+    __llm = ChatOpenAI(
         model='gpt-4o-mini',
         temperature=0.5,
         api_key=API_KEY
     )
 
     # 파서 생성
-    parser = StrOutputParser()
+    __parser = StrOutputParser()
 
     # 체인 생성
-    chain = prompt | llm | parser
+    __chain = __prompt | __llm | __parser
 
     # 챗봇 질문 요청
     @staticmethod
     def ask_chatbot(user_request):
-        response = ChatbotService.chain.invoke(user_request)
+        response = ChatbotService.__chain.invoke(user_request)
         return response
